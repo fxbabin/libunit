d23df9a3a8d97e2cf5dc910b9c4a0c99a81939da
François-Xavier Babin <francoisxavierbabin@PSD12198.local>
2018-09-28 00:16:31 +0200

    nearly finished

--- main.c
+++ main.c
@@ -34,10 +34,50 @@ void		tests_dump(t_test **list)
 	ft_putstr(" NULL\n");
 	*list = l;
 }
-
+void	exit_timeout(int sig)
+{
+	(void)sig;
+	exit(SIGALRM);
+}
 void	child_process(t_test *test)
 {
+	signal(SIGALRM, exit_timeout);
+	alarm(1);
 	exit((test->test() == 0) ? 0 : -1);
+}
+
+void	process_status(char *test_name, int status)
+{
+	if (WIFEXITED(status))
+	{
+		if (WEXITSTATUS(status) == 0)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, LGREEN, "OK", RESET);
+		else if (WEXITSTATUS(status) == SIGALRM)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, RED, "TIMEOUT", RESET);
+		else
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, LRED, "KO", RESET);
+	}
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == SIGSEGV)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, RED, "SEGV", RESET);
+		else if (WTERMSIG(status) == SIGBUS)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, RED, "BUSE", RESET);
+		else if (WTERMSIG(status) == SIGABRT)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, RED, "ABORT", RESET);
+		else if (WTERMSIG(status) == SIGFPE)
+			ft_printf("\t%-*s : [%k%s%k]\n", 10, test_name, RED, "FPE", RESET);
+	}
+}
+
+void	parent_process(t_test *test)
+{
+	int		status;
+	//int		pid;
+		
+	(void)test;
+	wait(&status);
+	process_status(test->name, status);
 }
 
 void	tests_run(t_test **testlst)
@@ -54,11 +94,11 @@ void	tests_run(t_test **testlst)
 	while ((*testlst))
 	{
 		pid = fork();
+		//ft_printf("pid %d\n", pid);
 		if (pid == 0)
 			child_process(*testlst);
-		/*else:
-			parent_process();*/
-		ft_printf("pid %d\n", pid);
+		else
+			parent_process(*testlst);
 		*testlst = (*testlst)->next;
 	}
 	*testlst = l;
@@ -66,22 +106,65 @@ void	tests_run(t_test **testlst)
 	
 }
 
-int		test(void)
+int		test_ok(void)
 {
-	ft_printf("test 0\n");
 	return (0);
 }
 
-int		test1(void)
+int		test_ko(void)
 {
-	ft_printf("test 1\n");
-	return (1);
+	//ft_printf("test ko\n");
+	return (-1);
 }
 
-int		test2(void)
+int		test_segv(void)
 {
-	ft_printf("%s%s%s\n", NULL);
-	return (1);
+	ft_printf("%s%s%s%s%s%s\n");
+	return (-1);
+}
+
+int		test_buse(void)
+{
+	char	*s;
+
+	s = "to";
+	ft_strcpy(s, "ffffffffffffffffff");
+	return (-1);
+}
+
+void handler(int sig)
+{
+	(void)sig;
+	//exit(SIGABRT);
+}
+
+int test_abort(void)
+{
+	abort();
+	//signal(SIGABRT,handler);
+	return (0);
+}
+
+int test_fpe(void)
+{
+	int		nb1;
+	int		nb2;
+	int		total;
+
+	nb1 = 5;
+	nb2 = 0;
+	total = nb1 / nb2;
+	//signal(SIGABRT,handler);
+	return (0);
+}
+
+int test_timeout(void)
+{
+	while (1)
+	{
+		;
+	}
+	return(0);
 }
 
 int		main(void)
@@ -89,8 +172,13 @@ int		main(void)
 	t_test		*lst;
 
 	lst = NULL;
-	test_add(&lst, "name_1", &test);
-	test_add(&lst, "name_2", &test2);
+	test_add(&lst, "test_ok", &test_ok);
+	test_add(&lst, "test_ko", &test_ko);
+	test_add(&lst, "test_segv", &test_segv);
+	test_add(&lst, "test_buse", &test_buse);
+	test_add(&lst, "test_abort", &test_abort);
+	test_add(&lst, "test_fpe", &test_fpe);
+	test_add(&lst, "test_timeout", &test_timeout);
 	tests_run(&lst);
 	
 	tests_del(&lst);
